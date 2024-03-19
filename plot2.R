a82eb032ede51cc848a2b72eebb8794d5bb5b0a9
gitpro2024 <157456591+gitpro2024@users.noreply.github.com>
2024-03-19 10:48:26 +0100

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,6 +1,6 @@
 # Read in and re-name electric power consumption data according to needs. Define the date subset time period of interest (two day window)
 
-power <- read.table("household_power_consumption.txt",skip=1,sep=";")
+power <- read.table("household_power_consumption.txt",  , na.strings="?")
 names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
 subpower <- subset(power,power$Date=="1/2/2007" | power$Date =="2/2/2007")
 
